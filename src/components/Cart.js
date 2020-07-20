5ad69eafc7b476ca4f45f291bb6372482e973242
triabagus <triatop9@gmail.com>
2020-07-20 14:47:34 +0700

    first commit

---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Cart extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h3>Cart</h3>
+            </div>
+         );
+    }
+}
+ 
+export default Cart;
