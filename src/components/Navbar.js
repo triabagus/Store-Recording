8cdc793522e2c7f94261d90ef7ee9027d772cbd9
triabagus <triatop9@gmail.com>
2020-07-20 15:18:11 +0700

    Navbar: add design navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../logo.svg';
 
 class Navbar extends Component {
+    
     render() { 
         return (  
-            <div>
-                <h3>Navbar</h3>
-            </div>
+            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5 bg-dark">
+                {/* 
+                https://www.iconfinder.com/icons/1243689/call_phone_icon
+                Creative Commons (Attribution 3.0 Unported);
+                https://www.iconfinder.com/Makoto_msk
+                */}
+                <Link to="/">
+                    <img src={Logo} alt="store" className="navbar-brand" style={{ width: "60px"}}/>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            Products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                    <button>
+                        <i className="fas fa-cart-plus" />
+                    </button>
+                </Link>
+            </nav>
         );
     }
 }
