77fa413d6b85a2fd5800b069d9b8d5f650820ca6
triabagus <triatop9@gmail.com>
2020-07-24 13:18:54 +0700

    Default: style page not found

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 
 class Default extends Component {
     render() { 
+        // console.log(this.props);
         return ( 
-            <div>
-                <h3>Page Not Found</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-purple text-uppercase pt-5">
+                        <h5 className="display-4">Page Not Found</h5>
+                        <div className="col-md-8 mx-auto text-center">
+                            <img src="img/404.svg" className="img-fluid" />
+                            <p className="mt-4">The request URL <span className="text-danger font-weight-bold">{this.props.location.pathname}</span>{" "}was not found.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
          );
     }
--- src/context.js
+++ src/context.js
@@ -113,8 +113,6 @@ class ProductProvider extends Component {
                 })
             
         }
-
-
     }
 
     removeItem = id => {
